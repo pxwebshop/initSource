49b13859d3bbc44e8cd43f421314f74ad4c52a33
congminhnguyen <minhcongnguyen11@gmail.com>
2022-03-12 03:57:40 +0700

    Done template for pc

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -1,3 +1,37 @@
-$(document).ready( ()=>{
-   
-});
+var openModalAK = document.getElementById('modalAK');
+var modalAK = document.getElementById('modal1');
+var closeModal1 = document.getElementById('btn-closeModal1');
+openModalAK.onclick = function() {
+    modalAK.style.display = "flex";
+    document.body.style.backgroundColor = "black";
+}
+closeModal1.onclick = function() {
+    modalAK.style.display = "none";
+    document.body.style.backgroundColor = "white";
+}
+
+
+var openModalTE = document.getElementById('modalTE');
+var modalTE = document.getElementById('modal2');
+var closeModal2 = document.getElementById('btn-closeModal2');
+openModalTE.onclick = function() {
+    modalTE.style.display = "flex";
+    document.body.style.backgroundColor = "black";
+}
+closeModal2.onclick = function() {
+    modalTE.style.display = "none";
+    document.body.style.backgroundColor = "white";
+}
+
+
+var openModalYM = document.getElementById('modalYM');
+var modalYM = document.getElementById('modal3');
+var closeModal3 = document.getElementById('btn-closeModal3');
+openModalYM.onclick = function() {
+    modalYM.style.display = "flex";
+    document.body.style.backgroundColor = "black";
+}
+closeModal3.onclick = function() {
+    modalYM.style.display = "none";
+    document.body.style.backgroundColor = "white";
+}
